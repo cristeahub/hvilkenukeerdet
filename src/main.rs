2fa85c187c2395cdced9cf0d37df4a0c6c1fad20
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2015-08-21 01:10:57 +0200

    Implement week range checker
    Now one can use `uke <week_number>` and this will print the dates for
    that week. The dates are for the current year.
    
    This closes #1.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate chrono;
 
 use std::env;
 use std::io::Read;
+use chrono::*;
 use hyper::Client;
 use hyper::header::Connection;
 
@@ -35,7 +36,36 @@ fn parse_string_to_int(s: String) -> usize {
 }
 
 fn show_week_period_for_week_number(w: usize) {
-    // TODO: implement the actual week number checker
+    let year = Local::now().year();
+    let mut start_date = UTC.ymd(year, 1, 1).and_hms(1,0,0);
+    let mut current_week = 1;
+
+    // week 1 is defined by the first week with a thursday
+    // in a year
+
+    let jan_1_week_day = start_date.weekday().number_from_monday();
+    if jan_1_week_day > 4 {
+        /* this week has no thursday */
+        /* the calculation will give the date of monday in the first week*/
+        start_date = UTC.ymd(year, 1, (1 + 8-jan_1_week_day)).and_hms(1,0,0);
+    } else {
+        /* start at this week's first day */
+        start_date = UTC.ymd(year-1, 12, (31 - (jan_1_week_day - 2))).and_hms(1,0,0);
+    }
+
+    while w != current_week {
+        start_date = start_date + Duration::weeks(1);
+        current_week += 1;
+    }
+
+    let end_date = start_date + Duration::days(6);
+
+    println!("Week {}, {} is from \"{}\" to \"{}\"",
+             w,
+             year,
+             start_date.format("%d %b"),
+             end_date.format("%d %b")
+            );
 }
 
 fn main() {
